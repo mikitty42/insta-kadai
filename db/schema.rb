42a045cf3c4960f57db2dd1483215ec8d24015c7
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-08-21 07:53:12 +0900

    UsersテーブルとPostsテーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_144240) do
+ActiveRecord::Schema.define(version: 2020_08_20_225153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_144240) do
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "picture"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_144240) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
