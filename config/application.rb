e500b74f372d744fff2ccbd29c9021962b281a18
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-08-20 22:24:32 +0900

    タイムゾーンの設定の変更

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,8 @@ module InstaClone
     config.generators do |g|
       g.assets false
       g.helper false
-    end
-
+    endconfig.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
