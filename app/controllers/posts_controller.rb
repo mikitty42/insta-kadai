c76eed10ce3e296bf6b28c0ef5cff24e14488c8a
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-08-21 00:09:55 +0900

    投稿機能の作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,58 @@
+class PostsController < ApplicationController
+  before_action :set_post,only: [:show,:edit,:update,:destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path,nocice: '投稿に失敗しました'
+      else
+        render :new
+      end
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.save
+      redirect_to posts_path,notice: '投稿を編集しました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path,notice: '投稿を削除しました'
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title,:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
