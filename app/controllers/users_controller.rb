9c80e5b89f3e9f97cf1db02e13d677ddfc5543e4
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-08-22 21:54:24 +0900

    User編集機能を作成しました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user,only:[:show,:edit,:update]
 
   def new
     @user = User.new
@@ -7,19 +8,31 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to user_path(@user.id),notice: 'user registration Succeeded in'
     else
       render :new
     end
   end
 
   def show
-    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id),notice: 'The user was edited successfully'
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name,:email,:password,:password_confirmation,:image,:image_cache)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
