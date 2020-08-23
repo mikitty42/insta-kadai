ef96ed478073a41731fd8a8599557863bf888190
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-08-23 22:09:09 +0900

    アクセス制限機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'users#new'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :favorites,only: [:create,:destroy,:index]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new,:create,:show,:edit,:update]
+  resources :users, only: [:index,:new,:create,:show,:edit,:update]
   resources :pictures do
     collection do
       post :confirm
